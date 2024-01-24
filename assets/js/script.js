f00845fda9a759a607a4553faa96ff8ecaa46f3d
Albert T <albert.terrazas@gmail.com>
2024-01-23 19:53:29 -0600

    Update script.js
    adding api not working.

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,24 +31,42 @@ addQuestion = ()=>{
 }
 showquestion = questions =>{
     const questionHTML = document.createElement('div');
-    questionHTML.classList.add();
+    questionHTML.classList.add('col-12');
 
-    questions.forEach(question => {
+    questions.forEach(question =>{
         rightAns = question.correct_answer;
 
         let possibleAnswers = question.incorrect_answers;
         possibleAnswers.splice(Math.floor(Math.random()*3),0,rightAns);
 
-    })
+        questionHTML.innerHTML= '<div class="row justify-content-between heading">
+        <p class="category">Category:${question.category}</p>
+        <div class="scores">
+        <span class="badge badge-primary">{rightNumber}</span>
+        <span class="badge badge-primary">{falseNumber}</span>
+        </div>
+        <div>
+        <h2 class="text-center">${question.question};
+
+      
+
     const answerDiv = document.createElement('div');
     answerDiv.classList.add('questions','row','justify-contecnt-around','mt-5');
+
     possibleAnswers.forEach(answer=>{
         const answerHTML = document.createElement('li');
         answerHTML.classList.add('col-12','col-md-5');
-        answerHTML.textContent=answer;
+        answerHTML.textContent = answer;
+        
+        selectAnswer
+        answerDiv.appendChild(answerHTML);
     })
 
-}
+    questionHTML.appendChild.apply(answerDiv);
+    document.querySelector('#app').appendChild
+})
+
+
 // class Question {
 //   constructor(question, choices, indexOfCorrectChoice) {
 //     this.question = question;
